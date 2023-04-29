de9950cadcbec71f6de4fc38351fe896b2bfadaf
0112358132134-2026 <68541@cag.edu.gt>
2023-04-28 23:35:29 -0600

    Ok!
    :)

---
+++ IoC _DI/IoC _DI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IoC__DI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ IoC _DI/IoC _DI/MyObject.cs
@@ -0,0 +1,24 @@
+﻿namespace IoC__DI
+{
+    public interface ITransient
+    { 
+    }
+
+    public interface IScoped
+    {
+    }
+
+    public interface ISingleton
+    {
+    }
+
+    public class MyObject : ITransient, IScoped, ISingleton
+    {
+        public readonly int Value;
+
+        public MyObject()
+        {
+            Value = new Random().Next(10000);
+        }
+    }
+}
